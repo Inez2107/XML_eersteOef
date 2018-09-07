482e4ace3278a80d28f0b1e6b5d8e7b83cb942e4
Inez DS <inezandries.eu@gmail.com>
2018-09-07 13:34:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws SAXException, IOException {
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newDefaultInstance();
+            StreamSource xsd = new StreamSource("boeken.xsd");
+            Schema schema = schemaFactory.newSchema(xsd);
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource("boeken.xml"));
+            System.out.println("Validatie is gelukt");
+        } catch (SAXException | IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
